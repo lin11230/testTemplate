9089a4eed3e232160376a0baecb1aa79d1158639
Eric Lin <lin11230@gmail.com>
2015-11-20 13:37:45 +0800

    add generate pdf procedure

--- testTemplate.go
+++ testTemplate.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/fatih/structs"
 	"github.com/hoisie/mustache"
@@ -41,5 +42,13 @@ func main() {
 	filelength, err := tmpHtmlFile.WriteString(str)
 	fmt.Printf("wrote %d bytes\n", filelength)
 	tmpHtmlFile.Sync()
+	cmd := "wkhtmltopdf"
+	args := []string{"--print-media-type", "--page-size", "A4", "-T", "0mm", "-B", "0mm", "-L", "0mm", "-R", "0mm", "--dpi", "600", "test_report.html", "test_report.pdf"}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully Generates the report of PDF format")
 
 }
